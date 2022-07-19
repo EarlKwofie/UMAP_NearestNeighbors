da4a58c7f0dd871abdf7cf6489a7bd4159f2aa3b
Joya Debi <joyaranidebi@gmail.com>
2022-07-19 17:11:00 -0400

    Get line between 2 images
    using sample dataset from yale pixplot.
    ran pixplot on dataset.
    ask user for start and end images
    parse path for image name
    extract coordinates from image name
    use numpy to get line
    
    need to confirm how json pairs connect to image.
    need to find closest distances to line

--- main.py
+++ main.py
@@ -1,98 +1,61 @@
-# This is a sample Python script.
-# Press the green button in the gutter to run the script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import json
-import math
 import os
-from PIL import Image
+from tracemalloc import start
+import numpy as np
+import matplotlib.pyplot as plt
 
-def printData(j_file):
-    #file = json.dumps(j_file);
-    print(j_file)
+#get image name from a path 
+def get_image_name(path):
+    list = path.split("/")
+    nameapos= list[-1]
+    imagename = nameapos[:-1]
+    return imagename
 
-f = open("test.txt", "w")
-f.write("Hello")
-f = open("test.txt", "r")
-file = f.read()
-f.close()
-
-printData(file)
-
-# simple method to convert json data to array of strings
-f_json = open("umap-e960482e-f0bc-11ec-a9d0-38fc985d69ec.json")
-f_data = json.load(f_json)
-#for i in f_data:
-    #print(i)
-
-# simple method to convert all the images in a file into an array and displays them in order
-f_images = []
-for image in os.listdir("./images/"):
-    if image.endswith(".jpg"):
-        f_images.append(image)
-
-for image in f_images:
-    image_file = os.path.basename(image)
-    display = Image.open("./images/" + image_file, 'r')
-    #display.show()
-
-# way to calculate the distance between 2 image indexes
-def getDistance(im_1, im_2):
-    image1_x = f_data[im_1][0]
-    image1_y = f_data[im_1][1]
-
-    image2_x = f_data[im_2][0]
-    image2_y = f_data[im_2][1]
-
-    diff_x = image1_x - image2_x
-    diff_y = image1_y - image2_y
-
-    square = diff_x * diff_x + diff_y * diff_y
-    return math.sqrt(square)
-
-print(getDistance(0,2))
-
-# Returns the 5 closest points on the UMAP to a point of focus on the UMAP
-def getNearestNeighbors(image_index):
-    distances = []
-    image_indexes = []
-    for i in range(25):
-        distances.append(getDistance(image_index, i))
-        image_indexes.append(i)
-
-        # for i in range(1, len(distances)):
-        #     key = distances[i]
-        #     j = i - 1
-        #     while j >= 0:
-        #         if distances[j] > key:
-        #             key_2 = distances[j]
-        #             i_key = image_indexes[i]
-        #             i_key_2 = image_indexes[j]
-        #
-        #             distances[i] = key_2
-        #             distances[j] = key
-        #
-        #             image_indexes[i] = i_key_2
-        #             image_indexes[j] = i_key
-        #             j -= 1
-        #         else:
-        #             continue
-
-    for i in range(25):
-        base = distances[i]
-        base_index = image_indexes[i]
-        j = i - 1
-        while j >= 0 and distances[j] > base:
-            distances[j + 1] = distances[j]
-            image_indexes[j + 1] = image_indexes[j]
-            j -= 1
-            distances[j + 1] = base
-            image_indexes[j + 1] = base_index
-
-    return image_indexes[1 : 6]
+def get_key(dict, val):
+    for key, value in dict.items():
+     if val == value:
+         return key
 
 
-print(getNearestNeighbors(5))
+#get file object
+fjson = open("output/data/layouts/umap-47646dc8-0787-11ed-b3ec-3af9d3c7419a.json", "r")
+#from file object get json object
+umapjson = json.load(fjson)
+
+#make dict of json objects
+indexjson = {}
+for i in range (0,len(umapjson)):
+    indexjson[i] = umapjson[i]
+
+#make dict of images 
+templistimages = os.listdir("output/data/originals")
+listimages = sorted(templistimages)
+indeximage = {}
+for i in range (0, len(listimages)):
+    indeximage[i] = listimages[i]
+
+#get starting coordinates
+startimagepath = input("Enter starting image :")
+startimage = get_image_name(startimagepath)
+indexstartimage = get_key(indeximage, startimage)
+startcoord = indexjson[indexstartimage]
+
+#get ending coordinates
+endimagepath = input("Enter ending image :")
+endimage = get_image_name(endimagepath)
+indexendimage = get_key(indeximage, endimage)
+endcoord = indexjson[indexendimage]
+
+x = [startcoord[0], endcoord[0]]
+y = [startcoord[1], endcoord[1]]
+
+coefficients = np.polyfit(x,y,1)
+#gives line in ax + b form
+polynomial = np.poly1d(coefficients)
+print(polynomial)
+
+
+
 
 
 
